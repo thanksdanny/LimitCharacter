a6af549baa1e0e41065a85bc8ffbcb76ad6ddf8d
thanksdanny <hj-07@163.com>
2016-03-27 23:27:38 +0800

    Initial Commit

---
+++ Limit Character/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Limit Character
+//
+//  Created by Danny Ho on 3/27/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
